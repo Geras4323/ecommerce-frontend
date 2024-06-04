7ebbc6398bc4978637f0932046c01d5601156a81
Geras4323 <german432yo@gmail.com>
2024-06-04 16:53:51 -0300

    fix(cart) no spam waring confirmed order modal
    - Spam warning
    - SVG colours

--- src/components/modals/cart.tsx
+++ src/components/modals/cart.tsx
@@ -1,26 +1,46 @@
 import type { OrderItem } from "@/functions/orders";
 import { Modal, type ModalProps } from "../layouts/modal";
 import Link from "next/link";
-import { CheckCircle2, PackageCheck } from "lucide-react";
+import { AlertTriangle, CheckCircle2, PackageCheck } from "lucide-react";
+import { useTheme } from "next-themes";
+import { cn } from "@/utils/lib";
 
 export function OrderConfirmationModal({
   isOpen,
-  // onClose,
   order,
   email,
 }: ModalProps & { order: OrderItem; email: string }) {
+  const { theme } = useTheme();
+
   return (
     <Modal isOpen={isOpen}>
       <div className="flex h-full w-full items-center gap-6">
         <div>
-          <CheckCircle2 className="size-16 text-success" />
+          <CheckCircle2
+            className={cn(
+              theme === "dark" ? "text-success" : "text-primary",
+              "size-12 sm:size-16"
+            )}
+          />
         </div>
         <div className="flex flex-col gap-2">
           <h3 className="text-2xl text-primary">Recibimos tu pedido</h3>
           <p className="w-full text-secondary">
             Te enviamos un mail a{" "}
-            <span className="text-primary/70">{email}</span> con los detalles de
-            tu orden
+            <span className="font-semibold text-primary/70">{email}</span> con
+            los detalles de tu orden
+          </p>
+          <p className="flex w-full items-center gap-2 text-secondary">
+            <AlertTriangle
+              className={cn(
+                theme === "dark" ? "text-warning" : "text-primary",
+                "size-4 min-w-4"
+              )}
+            />
+            <span>
+              Por favor comprobá tu bandeja de{" "}
+              <u className="underline-offset-2">spam</u>.
+            </span>
           </p>
         </div>
       </div>
