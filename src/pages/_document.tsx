e662c696a0cd3641ff55f2f0fd21b91630484a0a
Geras4323 <german432yo@gmail.com>
2024-06-14 17:25:04 -0300

    chore: add google verification file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="UaSQGCGW8p6zx_JtwvWD0DsxCFaJdKFM9GBaQA2QqII"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
